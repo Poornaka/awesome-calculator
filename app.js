de00366390a012517084d755b6aa432ccf6839c8
Poornaka Bandara <69359070+Poornaka@users.noreply.github.com>
2021-06-08 18:28:15 +0530

    Update app.js
    Disabled the operator to come at first

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 let one = document.getElementById("one");
 let decimal = document.getElementById("point");
 let input = document.getElementById("input");
-// let two = document.getElementById("two");
-// let three = document.getElementById("three");
-// let four = document.getElementById("four");
-// let five = document.getElementById("five");
-// let six = document.getElementById("six");
-// let seven = document.getElementById("seven");
-// let eight = document.getElementById("eight");
-// let nine = document.getElementById("nine");
 
 let beforeWasDecimal = []
 
